53e21ddc4ba8efd1562634471942befcba1b40a6
Angelica <angelica.benham23@gmail.com>
2023-09-01 15:06:34 +1200

    set the status bar colour

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace ZumbaMobileApp
 {
@@ -9,6 +10,7 @@ namespace ZumbaMobileApp
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit() // Initialize the .NET MAUI Community Toolkit
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
