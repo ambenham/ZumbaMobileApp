055b7e27ac624999bf55de26562e6166d4153339
Angelica <angelica.benham23@gmail.com>
2023-09-13 18:27:14 +1200

    added data classes for concessions

---
+++ Models/Concession.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZumbaMobileApp.Models
+{
+    class Concession
+    {
+        public int ConcessionID { get; set; }
+        public int ConcessionType { get; set; }
+        public int UserID { get; set; }
+        public DateTime DatePaid { get; set; }
+        public string Status { get; set; }
+        
+        public Concession(int concessionID, int concessionType, int userID, DateTime datePaid, string status)
+        {
+            this.ConcessionID = concessionID;
+            this.ConcessionType = concessionType;
+            this.UserID = userID;
+            this.DatePaid = datePaid;
+            this.Status = status;
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZumbaMobileApp.Models
+{
+    class User
+    {
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Passkey { get; set; }
+        public string Residence { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Age { get; set; }
+        public int PaymentMethodID { get; set; }
+        public int ConcessionID { get; set; }
+        public DateTime DateJoined { get; set; }
+        public DateTime? DateLeft { get; set; }
+
+        
+        public User(int userID, string firstName, string lastName, string email, string passkey, string residence, string phoneNumber, int age, int paymentMethodID, int concessionID, DateTime dateJoined)
+        {
+            this.UserID = userID;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Email = email;
+            this.Passkey = passkey;
+            this.Residence = residence;
+            this.PhoneNumber = phoneNumber;
+            this.Age = age;
+            this.PaymentMethodID = paymentMethodID;
+            this.ConcessionID = concessionID;
+            this.DateJoined = dateJoined;
+            this.DateLeft = null;
+        }
+    }
+}
